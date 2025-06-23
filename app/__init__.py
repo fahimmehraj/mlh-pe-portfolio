eac2d4d0056f4ab5eef6c6863fb13111937445c3
Fahim H <fahimhussain21@gmail.com>
2025-06-22 20:22:39 -0400

    Dynamically fetch all pages

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,19 @@ from .hobbies import hobby_list
 load_dotenv()
 app = Flask(__name__)
 
+pages = []
+print(os.getcwd())
+for filename in os.listdir(os.getcwd() + "/app/templates"):
+    if filename.endswith('.html') and filename != "index.html":
+        # from the 0th position to the position before 5 from the end,
+        # removing the .html extension
+        pages.append(filename[:-5])
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), experiences=experiences)
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), experiences=experiences, pages=pages)
 
 @app.route('/hobbies')
 def hobbies():
-    return render_template('hobbies.html', title="Hobbies", hobbies=hobby_list)
+    return render_template('hobbies.html', title="Hobbies", hobbies=hobby_list, pages=pages)
