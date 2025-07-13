59f94fae34d7167518523a6ac68fd400c2884dfd
Fahim <fahimhussain21@gmail.com>
2025-07-13 07:00:58 +0000

    use services instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,13 +1,7 @@
-tmux kill-session -t portfolio
 cd /root/mlh-pe-portfolio 
 git fetch && git reset origin/main --hard
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d -s portfolio "
-cd /root/mlh-pe-portfolio
-source .venv/bin/activate
-flask run --host=0.0.0.0
-"
-
+systemctl restart myportfolio
 
